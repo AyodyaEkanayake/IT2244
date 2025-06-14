8f3973b9c7da9f12e9d91998d24ecc6dd102b345
AyodyaEkanayake <ayodyae822@gmail.com>
2025-06-15 01:23:47 +0530

    Update bashcodes20all.c

--- bashcodes20all.c
+++ bashcodes20all.c
@@ -2,9 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h> //for read(), write(), 
+#include <sys/wait.h>
 #define MSGSIZE 16
-
 
 //name for the memory location (msg1, msg2, msg3)
 char* msg1 = "hello, world #1";
@@ -14,21 +14,25 @@ char* msg3 = "hello, world #3";
 int main(){
 	
 	char inbuf[MSGSIZE]; // inbuf is an array
-	int p[2], i; // p is array of size 2 to read and write //to define file descriptor('fd()') fd(1)=>write, fd(0)=>read
+	int p[2], pid, nbytes; // p is array of size 2 to read and write //to define file descriptor('fd()') fd(1)=>write, fd(0)=>read
 	
 	if(pipe(p) < 0)
 		exit(1);
 	
 	/* continued */
-	/* write pipe */
+	if((pid = fork()) > 0){
+		write(p[1], msg1, MSGSIZE); //write(filedescriptor, msg, msgSize)
+		write(p[1], msg2, MSGSIZE); 
+		write(p[1], msg3, MSGSIZE); 
 	
-	write(p[1], msg1, MSGSIZE); //write(filedescriptor, msg, msgSize)
-	write(p[1], msg2, MSGSIZE); 
-	write(p[1], msg3, MSGSIZE); 
-	
-	for(i=0; i<3; i++){
-		read(p[0], inbuf, MSGSIZE); //read(filedescriptor, msg, msgSize)
-		printf("%s\n", inbuf);
+		wait(NULL);
+	}
+	else{
+		while((nbytes = read(p[0], inbuf, MSGSIZE)) > 0)
+			printf("%s\n", inbuf);
+		if(nbytes != 0)
+			exit(2);
+		printf("Finished reading\n");
 	}
 	
 	return 0;
@@ -36,100 +40,232 @@ int main(){
 
 
 //Output
-
-// vi pipe.c
-// gcc pipe.c -o pipe
-// ./pipe
-
-//hello, world #1
-//hello, world #2
-//hello, world #3
+ pipe2.c
+ gcc pipe2.c -o pipe2
+ ./pipe2
+hello, world #1
+hello, world #2
+hello, world #3
 
 
 
 
-//Question
 
-/*
-1. Get three input from user (//parent process)
- name: 
- reg no: 
- age: 
 
-2. Print the output from the child(//child process)
- name: abc
- reg no: 2022/ict/01
- age: 25
-*/
+// Exercise (02)
+ step01: parent process get input from user
+ step02: sending these inputs to child process
+ step03: child process getting these inputs from parent and doing calculation
+ step04: step the calculated values to parent
+ step05: parent print the outputs
+ 
+ [circle,rectangle,triangle,square]
+ 
+ 
+// Expected Output
+Area calculation program
+
+Circle => C
+Triangle => T
+Square => S
+Rectangle => R
+
+Enter your choice: C
+
+Circle Area Calculation
+
+Enter the radius: 20.9
+Area is: 1371.58
 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
-#define MSGSIZE 16
+// Function declarations
+char get_shape_input();
+void get_dimensions(char shape, double *v1, double *v2);
+void send_inputs_to_child(int fd[], char shape, double v1, double v2);
+void calculate_area_in_child(int fd_read[], int fd_write[]);
+double receive_area_from_child(int fd[]);
 
 int main() {
-    char name[MSGSIZE], regno[MSGSIZE], age[MSGSIZE];
-    int p[2], f, i;
+    int fd1[2], fd2[2];
+    pipe(fd1); // Parent to child
+    pipe(fd2); // Child to parent
 
-    if (pipe(p) < 0) {
-        perror("Pipe failed");
-        exit(1);
-    }
+    pid_t pid = fork();
 
-    if ((f = fork()) == 0) {
-        // Child process
-        close(p[1]); // Close write end
-        for (i = 0; i < 3; i++) {
-            char inbuf[MSGSIZE];
-            read(p[0], inbuf, MSGSIZE);
-            printf("%s", inbuf);
-        } 
-        close(p[0]);
-    } else if (f > 0) {
+    if (pid > 0) {
         // Parent process
-        close(p[0]); // Close read end
-        printf("Name: ");
-        fgets(name, MSGSIZE, stdin);
-        printf("Reg No: ");
-        fgets(regno, MSGSIZE, stdin);
-        printf("Age: ");
-        fgets(age, MSGSIZE, stdin);
+        close(fd1[0]); // Close read end of pipe1
+        close(fd2[1]); // Close write end of pipe2
 
-        write(p[1], name, MSGSIZE);
-        write(p[1], regno, MSGSIZE);
-        write(p[1], age, MSGSIZE);
-        close(p[1]);
+        printf("Area calculation program\n\n");
+        printf("Circle => C\nTriangle => T\nSquare => S\nRectangle => R\n\n");
+
+        char shape = get_shape_input();
+
+        double v1 = 0.0, v2 = 0.0;
+        get_dimensions(shape, &v1, &v2);
+        send_inputs_to_child(fd1, shape, v1, v2);
+
+        double area = receive_area_from_child(fd2);
+        printf("Area is: %.2lf\n", area);
+
+        close(fd1[1]);
+        close(fd2[0]);
+        wait(NULL); // Wait for child process to finish
     } else {
-        perror("Fork failed");
-        exit(1);
+        // Child process
+        close(fd1[1]); // Close write end of pipe1
+        close(fd2[0]); // Close read end of pipe2
+
+        calculate_area_in_child(fd1, fd2);
+
+        close(fd1[0]);
+        close(fd2[1]);
+        exit(0);
     }
 
     return 0;
 }
 
+// Get shape input from user
+char get_shape_input() {
+    char shape;
+    printf("Enter your choice: ");
+    scanf(" %c", &shape);
+    return shape;
+}
+
+// Get dimensions based on shape
+void get_dimensions(char shape, double *v1, double *v2) {
+    switch (shape) {
+        case 'C':
+            printf("Circle Area Calculation\nEnter the radius: ");
+            scanf("%lf", v1);
+            break;
+        case 'T':
+            printf("Triangle Area Calculation\nEnter the base: ");
+            scanf("%lf", v1);
+            printf("Enter the height: ");
+            scanf("%lf", v2);
+            break;
+        case 'S':
+            printf("Square Area Calculation\nEnter the side: ");
+            scanf("%lf", v1);
+            break;
+        case 'R':
+            printf("Rectangle Area Calculation\nEnter the width: ");
+            scanf("%lf", v1);
+            printf("Enter the height: ");
+            scanf("%lf", v2);
+            break;
+        default:
+            printf("Invalid shape.\n");
+            exit(1);
+    }
+}
+
+// Send shape and dimensions to child
+void send_inputs_to_child(int fd[], char shape, double v1, double v2) {
+    write(fd[1], &shape, sizeof(shape));
+    write(fd[1], &v1, sizeof(v1));
+    write(fd[1], &v2, sizeof(v2));
+}
+
+// Calculate area in child and send to parent
+void calculate_area_in_child(int fd_read[], int fd_write[]) {
+    char shape;
+    double v1, v2, area;
+
+    read(fd_read[0], &shape, sizeof(shape));
+    read(fd_read[0], &v1, sizeof(v1));
+    read(fd_read[0], &v2, sizeof(v2));
+
+    switch (shape) {
+        case 'C':
+            area = 3.14159 * v1 * v1;
+            break;
+        case 'T':
+            area = 0.5 * v1 * v2;
+            break;
+        case 'S':
+            area = v1 * v1;
+            break;
+        case 'R':
+            area = v1 * v2;
+            break;
+        default:
+            area = -1.0;
+    }
+
+    write(fd_write[1], &area, sizeof(area));
+}
+
+// Receive area from child process
+double receive_area_from_child(int fd[]) {
+    double area;
+    read(fd[0], &area, sizeof(area));
+    return area;
+}
 
 
 
 //Output
+vi Area.c
+gcc CirTraiRectSqua.c -o CirTraiRectSqua
+./CirTraiRectSqua
 
-// vi pipeExercise.c
-// gcc pipeExercise.c -o pipeExercise
-// ./pipeExercise
+Area calculation program
 
-//Name: Ayo
-Reg No: 2021/ICT/61
-Age: 23
+Circle => C
+Triangle => T
+Square => S
+Rectangle => R
 
-Ayo
-2021/ICT/61
-23
+Enter your choice: C
+Circle Area Calculation
+Enter the radius: 7
+Area is: 153.94
+[nipunika@ChathuraDN ~]$ ./CirTraiRectSqua
+Area calculation program
 
+Circle => C
+Triangle => T
+Square => S
+Rectangle => R
 
+Enter your choice: T
+Triangle Area Calculation
+Enter the base: 6
+Enter the height: 12
+Area is: 36.00
+[nipunika@ChathuraDN ~]$ ./CirTraiRectSqua
+Area calculation program
 
+Circle => C
+Triangle => T
+Square => S
+Rectangle => R
 
+Enter your choice: S
+Square Area Calculation
+Enter the side: 10
+Area is: 100.00
+[nipunika@ChathuraDN ~]$ ./CirTraiRectSqua
+Area calculation program
 
+Circle => C
+Triangle => T
+Square => S
+Rectangle => R
 
-
+Enter your choice: R
+Rectangle Area Calculation
+Enter the width: 8
+Enter the height: 10
+Area is: 80.00
